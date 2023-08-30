24a832ae111fc5e15c921619f49617b67eec81d6
davidruiz-gnoss <davidruiz@gnoss.com>
2023-08-30 11:32:41 +0200

    Security Improvement

--- Gnoss.BackgroundTask.Mail/Program.cs
+++ Gnoss.BackgroundTask.Mail/Program.cs
@@ -3,6 +3,7 @@ using Es.Riam.Gnoss.AD.EntityModel;
 using Es.Riam.Gnoss.AD.EntityModelBASE;
 using Es.Riam.Gnoss.AD.Virtuoso;
 using Es.Riam.Gnoss.CL;
+using Es.Riam.Gnoss.CL.RelatedVirtuoso;
 using Es.Riam.Gnoss.Servicios;
 using Es.Riam.Gnoss.Util.Configuracion;
 using Es.Riam.Gnoss.Util.General;
